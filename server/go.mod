a736f0b26bb38cb43bebc53456d6819131949911
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2025-02-11 17:06:59 +0100

    feat(services): adds nixpacks_service.go

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,7 @@
 module github.com/overal-x/formatio
 
 go 1.23.4
+
+require github.com/samber/lo v1.49.1
+
+require golang.org/x/text v0.21.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/overal-x/formatio/services"
+)
 
 func main() {
-	fmt.Println("Hello, Freaking World!")
+	execService := services.NewExecService()
+	nixpacksService := services.NewNixpacksService(execService)
+
+	err := nixpacksService.Install(services.InstallArgs{
+		Callback: func(out *string, err error) {
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(*out)
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	appDirectory := "<test-project-directory>"
+	err = nixpacksService.Build(services.BuildArgs{
+		AppName:      "shortlet",
+		AppDirectory: appDirectory,
+		Env: &map[string]string{
+			"NIXPACKS_NODE_VERSION": "20",
+		},
+		Callback: func(out *string, err error) {
+			if err != nil {
+				panic(err)
+			}
+			if out != nil {
+				fmt.Println(*out)
+			}
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = nixpacksService.Run(services.RunArgs{
+		AppName: "shortlet",
+		Callback: func(out *string, err error) {
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(*out)
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
 }
